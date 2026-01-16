f2c8e4da5fd58052c35a078416469ca7561549b7
virgutiss-oss <virgutiss@icloud.com>
2026-01-16 15:17:36 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,31 @@
+SPORTS_DATA = {
+    "Football": {
+        "Premier League": [
+            "Arsenal", "Chelsea", "Liverpool", "Man City", "Man United"
+        ],
+        "La Liga": [
+            "Real Madrid", "Barcelona", "Atletico Madrid"
+        ],
+        "Serie A": [
+            "Inter", "AC Milan", "Juventus"
+        ]
+    },
+
+    "Basketball": {
+        "NBA": [
+            "Lakers", "Warriors", "Celtics", "Bucks"
+        ],
+        "EuroLeague": [
+            "Real Madrid", "Olympiacos", "Fenerbahce"
+        ]
+    },
+
+    "Tennis": {
+        "ATP": [
+            "Djokovic", "Alcaraz", "Medvedev"
+        ],
+        "WTA": [
+            "Swiatek", "Sabalenka", "Gauff"
+        ]
+    }
+}
