9c457df0a2087f702b103e6a87db43ca5ad3d41b
virgutiss-oss <virgutiss@icloud.com>
2026-01-16 15:20:52 +0200

    Update arbitrage.py

--- arbitrage.py
+++ arbitrage.py
@@ -1,12 +1,2 @@
-def find_arbitrage(books):
-    best_home = max(books, key=lambda x: x["home"])
-    best_away = max(books, key=lambda x: x["away"])
-
-    value = (1 / best_home["home"]) + (1 / best_away["away"])
-
-    return {
-        "exists": value < 1,
-        "value": round(value, 3),
-        "home_book": best_home["book"],
-        "away_book": best_away["book"]
-    }
+def check_arbitrage(home_odds, away_odds):
+    return (1/home_odds + 1/away_odds) < 1
