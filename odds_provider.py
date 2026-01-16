b2f806ef9adc1d7ad5d65d95dab82ef02f23077d
virgutiss-oss <virgutiss@icloud.com>
2026-01-16 19:09:41 +0200

    Update odds_provider.py

--- odds_provider.py
+++ odds_provider.py
@@ -1,11 +1,6 @@
-# ČIA BUS API VĖLIAU (Betfair / Pinnacle / OddsAPI)
-
 def get_odds(sport, home, away):
-    # mock odds (kad veiktų dabar)
     if sport == "Tennis":
         return {"home": 1.95, "away": 1.95}
-
     if sport == "Basketball":
         return {"home": 2.05, "away": 1.80}
-
     return {"home": 2.10, "away": 1.85}
