b93423834e4c4f9a34bedbb4ee80de6fcd8853e4
virgutiss-oss <virgutiss@icloud.com>
2026-01-16 19:12:17 +0200

    Create live_matches.py

---
+++ live_matches.py
@@ -0,0 +1,6 @@
+def get_live_matches():
+    return [
+        {"sport": "Football", "home": "Arsenal", "away": "Chelsea", "minute": 67},
+        {"sport": "Basketball", "home": "Lakers", "away": "Celtics", "minute": "Q3"},
+        {"sport": "Tennis", "home": "Djokovic", "away": "Alcaraz", "minute": "Set 2"}
+    ]
