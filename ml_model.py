df79ab88e80f0fc80dd47172c2447c12000b3e1d
virgutiss-oss <virgutiss@icloud.com>
2026-01-17 08:06:48 +0200

    Delete ml_model.py

--- ml_model.py
+++
@@ -1,8 +0,0 @@
-def predict_prob_by_sport(sport):
-    if sport == "Football":
-        return 0.52, 0.48
-    if sport == "Basketball":
-        return 0.55, 0.45
-    if sport == "Tennis":
-        return 0.60, 0.40
-    return 0.5, 0.5
