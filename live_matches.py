ee807319ef664b6e807cc2cfbfc3827e496b52f9
virgutiss-oss <virgutiss@icloud.com>
2026-01-17 08:05:28 +0200

    Delete live_matches.py

--- live_matches.py
+++
@@ -1,6 +0,0 @@
-def get_live_matches():
-    return [
-        {"sport": "Football", "home": "Arsenal", "away": "Chelsea", "minute": 67},
-        {"sport": "Basketball", "home": "Lakers", "away": "Celtics", "minute": "Q3"},
-        {"sport": "Tennis", "home": "Djokovic", "away": "Alcaraz", "minute": "Set 2"}
-    ]
