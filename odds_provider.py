a9d419edbf9d35f72a4ca4075fed2552e00fe60c
virgutiss-oss <virgutiss@icloud.com>
2026-01-17 08:06:22 +0200

    Delete odds_provider.py

--- odds_provider.py
+++
@@ -1,6 +0,0 @@
-def get_odds(sport, home, away):
-    if sport == "Tennis":
-        return {"home": 1.95, "away": 1.95}
-    if sport == "Basketball":
-        return {"home": 2.05, "away": 1.80}
-    return {"home": 2.10, "away": 1.85}
